fc6da09b7457726025a54312d01534ec0d1e6013
Joseph Chris <joseph@josephcz.xyz>
2022-03-31 00:01:45 -0700

    add requirements-dev.txt, manage dev dependencies by it

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 py -m venv venv-build
 call venv-build\Scripts\activate
-pip3 install setuptools wheel
 pip3 install -r requirements.txt
+pip3 install -r requirements-dev.txt
 setup.py sdist bdist_wheel
 for /D %%i in (*.egg-info) do rd /S /Q "%%i"
 rd /S /Q venv-build build
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m venv venv-build
 source ./venv-build/bin/activate
-pip3 install setuptools wheel
 pip3 install -r requirements.txt
+pip3 install -r requirements-dev.txt
 python3 setup.py sdist bdist_wheel
 rm -rf *.egg-info venv-build build
